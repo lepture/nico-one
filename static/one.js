14629a3bc8729a5d870f999a2791779f8c2a1f2f
Hsiaoming Yang <me@lepture.com>
2013-08-13 08:53:03 +0800

    Style for pagination

--- static/one.js
+++ static/one.js
@@ -1,12 +1,17 @@
-define(function(require, exports, module) {
-  var $ = require('gallery/jquery/1.8.2/jquery');
-  $('.entry-content h2, .entry-content h3, .entry-content h4, .entry-content h5').each(function(index, item) {
-    var $item = $(item);
-    var link = [
-      '<a class="title-permalink" title="Permalink to this title" href="#',
-      $item.attr('id'),
-      '">¶</a>'
-    ].join('');
-    $item.append(link);
-  });
-});
+function permalink() {
+  if (!document.querySelectorAll) {
+    return;
+  }
+  var hs = document.querySelectorAll('.entry-content h2, .entry-content h3, .entry-content h4, .entry-content h5');
+  for (var i=0; i < hs.length; i++) {
+    (function(item) {
+      var link = document.createElement('a');
+      link.className = 'title-permalink';
+      link.title = 'Permalink to this title';
+      link.href = '#' + item.id;
+      link.innerHTML = '¶';
+      item.appendChild(link);
+    })(hs[i]);
+  }
+}
+permalink();
